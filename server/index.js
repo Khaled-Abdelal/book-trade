f6aa8589272d6fd04f3d089e7f9c9482f5b05cfa
Khaled Abdelal <Khaled.Abdelal@outlook.com>
2019-06-15 06:49:01 +0200

    database connection set

--- server/index.js
+++ server/index.js
@@ -1,7 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true });
+mongoose.connection.on('error', err => {
+  // eslint-disable-next-line no-console
+  console.log(err);
+});
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
