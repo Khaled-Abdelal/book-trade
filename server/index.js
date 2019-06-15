a5543cb6f802ade3a635f25bbba0300c21b6f473
Khaled Abdelal <Khaled.Abdelal@outlook.com>
2019-06-16 00:50:16 +0200

    start oauth passport  flow

--- server/index.js
+++ server/index.js
@@ -1,18 +1,40 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const GoogleStratagy = require('passport-google-oauth20').Strategy;
 
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// ------------ MiddleWares----------//
+passport.use(
+  new GoogleStratagy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log(accessToken, 'accessToken');
+      console.log(refreshToken, 'refreshToken');
+      console.log(profile, 'profile');
+    }
+  )
+);
+
+// ------------- DB-Config---------//
 mongoose.connect(process.env.DB_URI, { useNewUrlParser: true });
 mongoose.connection.on('error', err => {
-  // eslint-disable-next-line no-console
   console.log(err);
 });
 
+// ----------------Routes----------//
+app.get('/auth/google', passport.authenticate('google', { scope: ['email', 'profile'] }));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
   console.log(`server up port ${PORT}`);
 });
