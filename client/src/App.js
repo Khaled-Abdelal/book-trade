cce30a8acb2e7598310e6f9c50b5811025b25c55
Khaled <khaled@pop-os.localdomain>
2019-06-24 09:44:27 +0200

    add browsebooks component to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
 import BookCarousel from "./components/BookCarousel/BookCarousel";
+import BrowseBooks from "./components/BrowseBooks/BrowseBooks";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <BookCarousel />
+      <BrowseBooks />
     </div>
   );
 }
