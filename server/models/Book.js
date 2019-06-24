5764ba6e72378bb1562317ac762789576263a3cd
Khaled Abdelal <Khaled.Abdelal@outlook.com>
2019-06-24 10:50:30 +0200

    add rating to books model

--- server/models/Book.js
+++ server/models/Book.js
@@ -6,8 +6,10 @@ const bookSchema = new Schema({
   title: { type: String, max: 1024 },
   authors: [String],
   description: String,
-  cover: String,
-  owner: { type: Schema.Types.ObjectId, ref: 'User', required: true }
+  cover: Object,
+  owner: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  averageRating: { type: Number },
+  ratingsCount: { type: Number }
 });
 
 const Book = mongoose.model('Book', bookSchema);
