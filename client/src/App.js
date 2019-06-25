984d6b10f0b424622780ee001afd7e40153c73a8
Khaled <khaled@pop-os.localdomain>
2019-06-25 18:41:47 +0200

    wire up auth context and add toggler

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import "./App.css";
 import Layout from "./components/Layout/Layout";
 import Home from "./components/Home/Home";
-
+import { AuthProvider, AuthDispatchContext } from "./context/auth.context";
+import Axios from "axios";
+const baseURL = process.env.REACT_APP_BASE_URL;
 function App() {
+  const authDispatch = useContext(AuthDispatchContext);
+  useEffect(() => {
+    const token = JSON.parse(localStorage.getItem("auth-token"));
+    if (!token) {
+      return authDispatch({ type: "noAuth" });
+    }
+    async function getAuthUser() {
+      try {
+        const user = await Axios.get(`${baseURL}/api/users/me`, {
+          headers: { Authorization: `Bearer ${token}` }
+        });
+        authDispatch({ type: "foundUser", payload: { user: user.data } });
+      } catch (err) {
+        authDispatch({ type: "noAuth" });
+      }
+    }
+    getAuthUser();
+  }, []);
   return (
     <div className="App">
       <Layout>
--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -2,13 +2,20 @@ import React from "react";
 import BookCarousel from "../BookCarousel/BookCarousel";
 import Owners from "../Owners/Owners";
 import NavBar from "../NavBar/NavBar";
+import { Container, Row, Col } from "reactstrap";
 function Layout({ children }) {
   return (
     <div>
       <NavBar />
       <BookCarousel />
-      {children}
-      <Owners />
+      <Container>
+        <Row>
+          <Col md="8">{children}</Col>
+          <Col md="4">
+            <Owners />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
