31086997a49301816b4b872bd34b25ce64bead55
Khaled <khaled@pop-os.localdomain>
2019-08-15 15:40:20 +0200

    add book details modal

--- client/src/components/BookList/BookList.js
+++ client/src/components/BookList/BookList.js
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Row, Col } from "reactstrap";
 import StarRatings from "react-star-ratings";
+import BookDetails from '../BookDetails/BookDetails'
+import useToggle from '../../hooks/useToggle'
 import Color from 'color'
 import "./BookList.scss";
 
 function BookList({ books }) {
-
+  const [modalValue, toggler] = useToggle();
+  const [bookInModal, toggleBookInModal] = useState(null)
   return (
     <Row className="BookList">
       {books.map(book => {
         let colorEmbtyColor = Color(book.dominantColor).alpha(.4).lighten(.7).string()
         let starRatedColor = Color(book.dominantColor).string()
         return (
-          <Col key={book._id} sm="6" xs="12" md="4" className="BookList-single">
+          <Col key={book._id} sm="6" xs="12" md="4" className="BookList-single" onClick={() => { toggleBookInModal(book); toggler() }}>
 
             <img className="BookList-thumbnail" src={book.cover.thumbnail} alt={book.title} />
 
@@ -33,8 +36,10 @@ function BookList({ books }) {
           </Col>
         );
       })}
+      {bookInModal && <BookDetails book={bookInModal} modalValue={modalValue} toggler={toggler} />}
     </Row>
   );
 }
 
 export default BookList;
+
