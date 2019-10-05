aa3d81cc46dbf2af3b65aab88e99a5da516fb0ea
Khaled Abdelal <Khaled.Abdelal@outlook.com>
2019-10-05 08:54:36 +0200

    add loading spinners and minor tweaks

--- client/src/components/Owners/Owners.js
+++ client/src/components/Owners/Owners.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Container } from 'reactstrap'
+import { Container, Spinner } from 'reactstrap'
 import { Link } from 'react-router-dom'
 import './Owners.scss'
 
@@ -8,12 +8,16 @@ const baseURL = process.env.REACT_APP_BASE_URL;
 
 function Owners() {
   const [owners, setOwners] = useState([]);
+  const [loading, setLoading] = useState(false)
   useEffect(() => {
     async function getUsers() {
       try {
+        setLoading(true)
         const users = await axios.get(`${baseURL}/api/users?limit=6`);
         setOwners(users.data);
+        setLoading(false)
       } catch (err) {
+        setLoading(false)
         console.log(err);
       }
     }
@@ -24,7 +28,7 @@ function Owners() {
       <Container>
         <h3 className="Owners-title">Owners</h3>
         <ul className="Owners-list">
-          {owners.map(owner => {
+          {loading ? <Spinner color="" secondery /> : owners.map(owner => {
             return (
               <li className="Owners-owner" key={owner._id}>
                 <img className="avatar" src={owner.photo} alt={owner.name} />
